0c661e14dfaaa588c8933dfceeb04f9400c618d7
Eljosevier <105233110+RexCats@users.noreply.github.com>
2024-07-22 15:15:43 -0400

    Update settings.py

--- eva34/eva34/settings.py
+++ eva34/eva34/settings.py
@@ -66,8 +66,7 @@ ROOT_URLCONF = 'eva34.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
